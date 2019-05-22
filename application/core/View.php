a11925b85df304ced5bb5996cb4c768e31bb6c23
dylanrenwick <skidsirongenesisdev@gmail.com>
2019-05-22 10:28:38 -0400

    Simplified checkForActiveControllerAndAction in View

--- application/core/View.php
+++ application/core/View.php
@@ -132,19 +132,12 @@ class View
      */
     public static function checkForActiveControllerAndAction($filename, $navigation_controller_and_action)
     {
-        $split_filename = explode("/", $filename);
-        $active_controller = $split_filename[0];
-        $active_action = $split_filename[1];
-
         $split_filename = explode("/", $navigation_controller_and_action);
         $navigation_controller = $split_filename[0];
         $navigation_action = $split_filename[1];
 
-        if ($active_controller == $navigation_controller AND $active_action == $navigation_action) {
-            return true;
-        }
-
-        return false;
+        return View::checkForActiveController($filename, $navigation_controller)
+            && View::checkForActiveAction($filename, $navigation_action);
     }
 
     /**
